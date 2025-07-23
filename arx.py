b2f3142a208fff187bba04348bc455b2a22a1e2b
Vladimir-sama <vladimir.sama.alexandre@gmail.com>
2025-07-23 16:01:10 -0300

    NT platform compatibility fixes / attempt (Untested)

--- arx.py
+++ arx.py
@@ -7,7 +7,7 @@ from arx_lib.helpers import debug_print
 version_string : str = '[alpha]'
 
 import os, sys, subprocess, shutil, platform
-from typing import List, Tuple, Set, Dict
+from typing import List, Tuple, Set, Dict, Optional
 
 
 file_dir : str = os.path.dirname(os.path.realpath(__file__))
@@ -50,26 +50,34 @@ def build(file_in:str) -> None:
 
     compiler.add_c_main()
 
+    is_windows : bool = os.name == 'nt'
+    llc_path : Optional[str] = shutil.which('llc')
+    gcc_path : Optional[str] = shutil.which('gcc')
+
+    if (not llc_path) or (not gcc_path):
+        raise EnvironmentError('Make sure (llc) and (gcc) are installed and on your PATH.')
+
     os.makedirs(os.path.join(executable_dir, 'build'), exist_ok=True)
     os.makedirs(os.path.join(executable_dir, 'out'), exist_ok=True)
 
+    object_extension : str = '.obj' if is_windows else '.o'
     out_ll : str = os.path.join(executable_dir, 'build', os.path.basename(file_in).rsplit('.', 1)[0] + '.ll')
-    out_o : str = out_ll.rsplit('.', 1)[0] + '.o'
+    out_o : str = out_ll.rsplit('.', 1)[0] + object_extension
     with open(out_ll, 'w') as f:
         f.write(str(compiler.module))
     debug_print('[llc]')
-    subprocess.run([shutil.which('llc'), out_ll, '-filetype=obj', '-o', out_o])
+    subprocess.run([llc_path, out_ll, '-filetype=obj', '-o', out_o])
     
-    final_command : List[str] = [shutil.which('gcc'), out_o]
+    final_command : List[str] = [gcc_path, out_o]
     total_libs : int = len(compiler.extern_c) + 1
     for i, c_lib in enumerate(compiler.extern_c):
         print(f'[ {i + 1}/{total_libs} ]' + ' [lib] (' + c_lib + ')')
-        subprocess.run([shutil.which('gcc'), '-c', '-o', os.path.join(executable_dir, 'build', c_lib + '.o'), os.path.join(executable_dir, 'c_lib', c_lib + '.c')])
+        subprocess.run([gcc_path, '-c', '-o', os.path.join(executable_dir, 'build', c_lib + object_extension), os.path.join(executable_dir, 'c_lib', c_lib + '.c')])
         final_command.append(
-            os.path.join(executable_dir, 'build', c_lib + '.o')
+            os.path.join(executable_dir, 'build', c_lib + object_extension)
         )
     print(f'[ {total_libs}/{total_libs} ]' + ' [main]')
-    out_executable : str = os.path.join(executable_dir, 'out', os.path.basename(file_in).rsplit('.', 1)[0])
+    out_executable : str = os.path.join(executable_dir, 'out', os.path.basename(file_in).rsplit('.', 1)[0] + ('.exe' if is_windows else ''))
     subprocess.run(final_command + ['-o', out_executable])
     print('Built at: ' + out_executable)
 
