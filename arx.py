5a9e3f793cea62812f9e5a10d64033cdfe465bb0
Vladimir-sama <vladimir.sama.alexandre@gmail.com>
2025-09-13 01:06:48 -0300

    Added Chocolatey install

--- arx.py
+++ arx.py
@@ -4,11 +4,12 @@ from arx_lib.compiler import ArtemisCompiler
 from arx_lib.data_classes import ArtemisData
 from arx_lib.helpers import debug_print
 
-version_string : str = '[alpha]'
+version_string : str = '[2025.09.11]'
 
 import os, sys, subprocess, shutil, platform
 from typing import List, Tuple, Set, Dict, Optional
 
+is_windows : bool = os.name == 'nt'
 
 file_dir : str = os.path.dirname(os.path.realpath(__file__))
 frozen_dir = os.path.dirname(sys.executable)
@@ -66,7 +67,6 @@ def build(file_in:str) -> None:
 
     compiler.add_c_main()
 
-    is_windows : bool = os.name == 'nt'
     llc_path : Optional[str] = shutil.which('llc')
     gcc_path : Optional[str] = shutil.which('gcc')
 
@@ -125,9 +125,23 @@ if __name__ == '__main__':
                         exit(0)
                     else:
                         raise FileNotFoundError(f'Input file not found at [ {sys.argv[2]} ]')
+            case 'install':
+                if is_windows:
+                    chocolatey_path : Optional[str] = shutil.which('choco')
+                    if (not chocolatey_path):
+                        raise EnvironmentError('Make sure (choco) is installed and on your PATH.')
+                    subprocess.run([chocolatey_path, 'install', 'llvm', '-y'], check=True)
+                    subprocess.run([chocolatey_path, 'install', 'mingw', '-y'], check=True)
+                    subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'], check=True)
+                    subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', os.path.abspath('requirements.txt')], check=True)
+                    print('Windows install success')
+                else:
+                    print('Linux not implemented')
+                exit(0)
         
     print('Usage for (arx)')
     print('- arx version')
     print('- arx environment')
+    print('- arx install')
     print('- arx build <input.arx>')
     print('- arx insight')
